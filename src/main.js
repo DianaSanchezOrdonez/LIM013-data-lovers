b4b9df5e5f31d16cde1d144442881a2d62f28734
Diana Sánchez Ordóñez <dianaordonez1998@gmail.com>
2020-08-17 08:52:26 -0500

    trabajando con la data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/lol/lol.js';
 
-console.log(example, data);
+//const legends = data.name;
+//console.log(data.data['Aatrox'].blurb)
+
+const getLegends = () => {
+    let datos = data.data;
+    //let legends = data.data[id]
+
+    for ( const prop in datos){
+        console.log(datos[prop].blurb)
+    }
+    console.log('datos',datos)
+    //console.log('legends',legends)
+} 
+
+getLegends()
+//console.log('data',getLegends('Aatrox'))
+
+//console.log(example, data);
