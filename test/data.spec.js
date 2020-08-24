6c57f898ffd4a0a352a50e6d3743b9bdd1aabb87
Diana Sánchez Ordóñez <dianaordonez1998@gmail.com>
2020-08-24 17:28:07 -0500

    test del metodo nameChampionAz listo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,24 +1,40 @@
 import order from '../src/data.js';
 
-
-describe('example', () => {
+describe('Filtro del Array de Legend', () => {
   it('is a function', () => {
     expect(typeof order.filterLegend).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(filterLegend('Aatrox','Mage')).toBe(true);
+  it('returns `Aatrox`', () => {
+    const filter =[{name:"Aatrox",tags:"Mage"},{name:"Ahri",tags:"Assassin"}]
+    const resultFilter=[{name:"Aatrox",tags:"Mage"}]
+    expect(order.filterLegend(filter, "Mage")).toEqual(resultFilter);
   });
+  
 });
 
 
-/* describe('anotherExample', () => {
-  it.skip('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('Orden de A-Z', () => {
+  it('is a function nameChampionAz', () => {
+    expect(typeof order.nameChampionAz).toBe('function');
   });
 
-  it.skip('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `Ana,Belen,Camila`', () => {
+    const data = [{name:'Belen'},{name:'Camila'},{name:'Ana'}];
+    const dataAz = [{name:'Ana'},{name:'Belen'},{name:'Camila'}];
+    expect(order.nameChampionAz(data)).toEqual(dataAz);
   });
+
+  it('returns `Aatrox,Leona,Malphite,Zyra`', () => {
+    const data = [{name:'Zyra'},{name:'Leona'},{name:'Malphite'},{name:'Aatrox'}];
+    const dataAz = [{name:'Aatrox'},{name:'Leona'},{name:'Malphite'},{name:'Zyra'}];
+    expect(order.nameChampionAz(data)).toEqual(dataAz);
+  });
+
+  it('returns `Aatrox,leona,Malphite,zyra`', () => {
+    const data = [{name:'zyra'},{name:'Leona'},{name:'malphite'},{name:'Aatrox'}];
+    const dataAz = [{name:'Aatrox'},{name:'Leona'},{name:'malphite'},{name:'zyra'}];
+    expect(order.nameChampionAz(data)).toEqual(dataAz);
+  });
+
 });
- */
